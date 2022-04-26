a82f5024a41e2e189cc8afa509a13546fa423f3c
mosta <mostafakhaled835@outlook.com>
2022-04-26 17:35:42 +0300

    initial commit

--- src/com/team42/App.java
+++ src/com/team42/App.java
@@ -1,31 +1,35 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // Create users
-        User user1 = new User("m.kira@innopolis.ru", "12345", UserType.CUSTOMER);
-        User user2 = new User("a.huraira@innopolis.ru", "1234", UserType.ROAD_TRANSPORT_ADMIN);
-        User user3 = new User("v.ivanov@innopolis.ru", "125", UserType.CUSTOMER);
-
-        Package package1 = new Package(1, Location.PICKUP_POINT);
-
+        // Creating an object of database
         Database database = new Database();
 
-        // Adding some users
+        // Create users with different rules and storing them in the data base (Dummy
+        // data)
+        User user1 = new User("m.kira@innopolis.ru", "12345", UserType.DISTRIBUTION_CENTER_ADMIN);
+        User user2 = new User("a.huraira@innopolis.ru", "1234", UserType.AIRPORT_ADMIN);
+        User user3 = new User("v.ivanov@innopolis.ru", "125", UserType.ROAD_TRANSPORT_ADMIN);
+        User user4 = new User("z.vladimir@innopolis.ru", "123", UserType.PICKUP_POINT_ADMIN);
+        User user5 = new User("n.amir@innopolis.ru", "145", UserType.CUSTOMER);
+
+        // Storing the dummy data in the database
         database.addUser(user1);
         database.addUser(user2);
         database.addUser(user3);
+        database.addUser(user4);
+        database.addUser(user5);
 
-        // Adding Packages
-        database.addPackage(package1);
-
+        // Creating a handler to validate the user email, password, and checking its
+        // rule
         Handler handler = new ValidateUserExistsHandler();
-        handler.setNextHandler(new ValidatePasswordHandler()).setNextHandler(new RoleCheckHandler());
+        handler.setNextHandler(new ValidatePasswordHandler());
+        handler.setNextHandler(new RoleCheckHandler());
 
-        // The user will enter its login info and the number of the package to get its
-        // location
+        // Creating an authentication service and passing the handle we created to it
         AuthenticationService service = new AuthenticationService(handler);
-        service.login("a.huraira@innopolis.ru", "1234", UserType.ROAD_TRANSPORT_ADMIN, 1, Location.TRUCK);
-        service.login("m.kira@innopolis.ru", "12345", UserType.CUSTOMER, 1, null);
+
+        // The distribution
+        service.login("m.kira@innopolis.ru", "12345", UserType.DISTRIBUTION_CENTER_ADMIN);
 
     }
 }
