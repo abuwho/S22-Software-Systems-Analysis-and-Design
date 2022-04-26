7256c6e98161c4f992cdd6fd2ded4b195d7e47d7
mosta <mostafakhaled835@outlook.com>
2022-04-27 00:36:20 +0300

    initial Commit

--- src/com/team42/HandlerInterface.java
+++ src/com/team42/HandlerInterface.java
@@ -7,7 +7,7 @@ public interface HandlerInterface {
      * UserType: When the user login, They tries to login under a specific rule,
      * this rule is directly passed to the function here
      * Package Number: this contains the number of package that the user wants to
-     * add, updateLocation, or checkLocation (The action will be setermined
+     * add, updateLocation, or checkLocation (The action will be determined
      * according to the user role)
      * Location: This field is required only by staff members. It is used to add the
      * current location of the package to the database
