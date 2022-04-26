9949d601ca6bd6cef309b033bef3c2093f3e0cbd
mosta <mostafakhaled835@outlook.com>
2022-04-26 13:44:43 +0300

    initial commit

--- src/com/team42/App.java
+++ src/com/team42/App.java
@@ -4,11 +4,13 @@ public class App {
         // Create users
         User user1 = new User("m.kira@innopolis.ru", "12345", UserType.STAFF);
         User user2 = new User("a.huraira@innopolis.ru", "1234", UserType.CUSTOMER);
+        User user3 = new User("v.ivanov@innopolis.ru", "125", UserType.CUSTOMER);
 
         Database database = new Database();
 
         database.addUser(user1);
         database.addUser(user2);
+        database.addUser(user3);
 
         Handler handler = new ValidateUserExistsHandler()
                 .setNextHandler(new ValidatePasswordHandler())
